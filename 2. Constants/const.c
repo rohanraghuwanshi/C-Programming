2a2e165a0bb991b4a7e5eaa46da00bc97cf2bf59
Rohan Raghuvanshi <rohanraghuvanshi001@gmail.com>
2020-01-28 22:49:36 +0530

    Added how to use constants

---
+++ 2. Constants/const.c
@@ -0,0 +1,11 @@
+// Program to show the use of const keyword
+#include <stdio.h>
+
+int main() {
+
+   const char grade = 'a';
+   
+   printf("%c", grade);
+
+   return 0;
+}
---
+++ 2. Constants/define.c
@@ -0,0 +1,11 @@
+// program to show the use of define preprocessor
+#include <stdio.h>
+
+#define NAME 'C Programming'
+
+int main() {
+
+   printf("%s", NAME);
+
+   return 0;
+}
